d1d0420d5e3e5fad3bf3714d908a971344fdce81
Raghu K <raghuk.vit@gmail.com>
2026-03-09 07:49:56 +0530

    Shell scripts

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,21 @@
 echo Hello World
+
+#echo -e \\e[COLCODEmHello World\\e[0m
+# or
+# echo -e "\e[COLCODEmHelloWorld\e[0m"
+
+echo -e \\e[31mHello World\\e[0m
+echo -e \\e[32mHello World\\e[0m
+echo -e \\e[33mHello World\\e[0m
+echo -e \\e[34mHello World\\e[0m
+echo -e \\e[35mHello World\\e[0m
+echo -e \\e[36mHello World\\e[0m
+
+
+echo -e \\e[41mHello World\\e[0m
+echo -e \\e[42mHello World\\e[0m
+echo -e \\e[43mHello World\\e[0m
+echo -e \\e[44mHello World\\e[0m
+echo -e \\e[45mHello World\\e[0m
+echo -e \\e[46mHello World\\e[0m
+
